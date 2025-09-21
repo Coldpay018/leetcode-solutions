cf71386c504cb17e5a83965e3d31a0333c357da8
Tanish Bhardwaj <tanishbhardwaj21@gmail.com>
2025-09-21 12:47:38 +0530

    Add C++ solution for 0001 Two Sum (map lookup)

---
+++ cpp/0001-two-sum/Solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mp;
+        for (int i = 0; i < (int)nums.size(); i++) {
+            if (mp.find(target - nums[i]) != mp.end()) {
+                return {mp[target - nums[i]], i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
